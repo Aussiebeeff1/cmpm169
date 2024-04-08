fc3c6cb044b2cb311c6953e286419385766109e7
Aussiebeeff1 <133194849+Aussiebeeff1@users.noreply.github.com>
2024-04-07 17:05:05 -0700

    updating project.js
    updating the format

--- experiment1/js/project.js
+++ experiment1/js/project.js
@@ -31,12 +31,12 @@ Attention All Rat Residents!
 
 Dear $adventurer,
 
-We regret to inform you of a $disturbing development in our community. Reports have surfaced regarding a $mysterious series of rat $murders that have been taking place in various parts of our $neighborhood. 
-Details are still $scarce, but it appears that these incidents are not isolated and may be connected to a $larger, more sinister plot. Remain vigilant and take necessary precautions to ensure their safety and that of their loved ones.
+We regret to inform you of a $disturbing development in our community. 
+Reports have surfaced regarding a $mysterious series of rat $murders that have been taking place in various parts of our $neighborhood. 
+Details are still $scarce, but it appears that these incidents are not isolated and may be connected to a $larger, more sinister plot.
+Remain vigilant and take necessary precautions to ensure their safety and that of their loved ones.
 
 Here's an $item for your journey!
-
-
 `;
 
 
